ca1ab9f585cc448e6b615f8cbb8b4410a2bc5752
Om Sharma <153528804+R0GDEV@users.noreply.github.com>
2025-01-03 12:13:04 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-server: {
-    historyApiFallback: true,
-  },
   plugins: [react()],
-})
-
+  server: {
+    historyApiFallback: true, // Enables fallback for React Router
+  },
+});
